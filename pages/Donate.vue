e56a9b3f4974654bac6b56963c6a3248e831486a
TacticalWolves <arunvl88@gmail.com>
2022-12-08 15:53:16 -0800

    Re adding donate.vue

---
+++ pages/Donate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Donate with Bitcoin</h1>
+    <p>
+      Thank you for considering a donation to support our work. You can use
+      Bitcoin to make a secure and anonymous donation to our organization.
+    </p>
+    <a href="bitcoin:{{ bitcoinAddress }}?amount={{ donationAmount }}">
+      Donate with Bitcoin
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bitcoinAddress: '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2',
+      donationAmount: 0.01
+    }
+  }
+}
+</script>
+
+<style>
+</style>
